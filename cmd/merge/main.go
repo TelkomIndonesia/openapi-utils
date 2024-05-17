97ba6c9c0643a31300ba715b01cad12e7c4c61ed
Rucciva <rucciva@gmail.com>
2024-05-17 14:45:14 +0700

    write to file

--- cmd/merge/main.go
+++ cmd/merge/main.go
@@ -12,10 +12,14 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatalf("Usage: %s <path-to-main-spec>\n", os.Args[0])
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <path-to-main-spec> <path-to-new-spec>\n", os.Args[0])
 	}
 
+	dst := ""
+	if len(os.Args) > 2 {
+		dst = os.Args[2]
+	}
 	s := os.Args[1]
 	sf, _ := filepath.Abs(s)
 	specDir, _ := filepath.Abs(filepath.Dir(s))
@@ -42,5 +46,15 @@ func main() {
 		log.Fatalln("fail to bundle openapi spec:", err)
 	}
 
-	os.Stdout.Write(bytes)
+	if dst == "" {
+		if _, err := os.Stdout.Write(bytes); err != nil {
+			log.Fatalln("fail to write stdout:", err)
+		}
+		return
+	}
+
+	if err := os.WriteFile(dst, bytes, 0644); err != nil {
+		log.Fatalln("fail to write file:", err)
+	}
+
 }
