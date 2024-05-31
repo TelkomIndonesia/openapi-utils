80a67eaea687ab49e59827646c75647f3b6c8bf4
Rucciva <rucciva@gmail.com>
2024-05-31 21:59:37 +0700

    refactor

--- cmd/proxy/internal/proxy/util.go
+++ cmd/proxy/internal/proxy/util.go
@@ -1,34 +1,72 @@
 package proxy
 
 import (
-	"encoding/json"
-	"fmt"
-	"strconv"
+	"strings"
 
-	"gopkg.in/yaml.v3"
+	v3 "github.com/pb33f/libopenapi/datamodel/high/v3"
 )
 
-func printYAMLNode(name string, y ...*yaml.Node) {
-	fmt.Println(name)
-	for i, y := range y {
-		v := map[string]interface{}{}
-		y.Decode(&v)
-		b, _ := json.Marshal(v)
-		fmt.Println(strconv.Itoa(i), ": ", string(b))
+func getOperationsMap(pi *v3.PathItem) (ops map[string]*v3.Operation) {
+	ops = map[string]*v3.Operation{}
+	if pi.Get != nil {
+		ops["Get"] = pi.Get
 	}
-	fmt.Println()
-}
-
-type renderable interface {
-	Render() ([]byte, error)
-}
-
-func copyRenderable[T renderable](t T) (n T, err error) {
-	b, err := t.Render()
-	if err != nil {
-		return n, err
+	if pi.Delete != nil {
+		ops["Delete"] = pi.Delete
 	}
-
-	err = yaml.Unmarshal(b, &n)
+	if pi.Post != nil {
+		ops["Post"] = pi.Post
+	}
+	if pi.Patch != nil {
+		ops["Patch"] = pi.Patch
+	}
+	if pi.Options != nil {
+		ops["Options"] = pi.Options
+	}
+	if pi.Head != nil {
+		ops["Head"] = pi.Head
+	}
+	if pi.Trace != nil {
+		ops["Trace"] = pi.Trace
+	}
 	return
+}
+
+func getOperation(pi *v3.PathItem, method string) *v3.Operation {
+	switch {
+	case strings.EqualFold("Get", method):
+		return pi.Get
+	case strings.EqualFold("Delete", method):
+		return pi.Delete
+	case strings.EqualFold("Post", method):
+		return pi.Post
+	case strings.EqualFold("Patch", method):
+		return pi.Patch
+	case strings.EqualFold("Options", method):
+		return pi.Options
+	case strings.EqualFold("Head", method):
+		return pi.Head
+	case strings.EqualFold("Trace", method):
+		return pi.Trace
+	}
+	return nil
+}
+
+func setOperation(pi *v3.PathItem, method string, val *v3.Operation) {
+	switch {
+	case strings.EqualFold("Get", method):
+		pi.Get = val
+	case strings.EqualFold("Delete", method):
+		pi.Delete = val
+	case strings.EqualFold("Post", method):
+		pi.Post = val
+	case strings.EqualFold("Patch", method):
+		pi.Patch = val
+	case strings.EqualFold("Options", method):
+		pi.Options = val
+	case strings.EqualFold("Head", method):
+		pi.Head = val
+	case strings.EqualFold("Trace", method):
+		pi.Trace = val
+	}
 }
