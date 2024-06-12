c28f3ab020f38b49fe354f13d6045cc491b1efc6
Rucciva <rucciva@gmail.com>
2024-06-12 14:17:46 +0700

    generate client

--- cmd/proxy/internal/proxy/generate.go
+++ cmd/proxy/internal/proxy/generate.go
@@ -10,6 +10,8 @@ import (
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/oapi-codegen/oapi-codegen/v2/pkg/codegen"
+	"github.com/pb33f/libopenapi"
+	v3 "github.com/pb33f/libopenapi/datamodel/high/v3"
 )
 
 func Generate(ctx context.Context, specPath string) (err error) {
@@ -18,26 +20,24 @@ func Generate(ctx context.Context, specPath string) (err error) {
 		return fmt.Errorf("fail to create proxy extension: %w", err)
 	}
 
-	spec, _, _, err := pe.CreateProxyDoc()
-	if err != nil {
-		return fmt.Errorf("fail to create proxy doc: %w", err)
-	}
-
-	kinspec, err := loadKinDoc(spec)
-	if err != nil {
-		return fmt.Errorf("fail to reload proxy doc with kin: %w", err)
-	}
-
 	t, err := loadTemplates()
 	if err != nil {
 		return fmt.Errorf("fail to load template: %w", err)
 	}
+
 	{
+		spec, _, _, err := pe.CreateProxyDoc()
+		if err != nil {
+			return fmt.Errorf("fail to create proxy doc: %w", err)
+		}
+		kinspec, err := loadKinDoc(spec)
+		if err != nil {
+			return fmt.Errorf("fail to reload proxy doc with kin: %w", err)
+		}
 		code, err := codegen.Generate(kinspec, codegen.Configuration{
 			PackageName: "oapi",
 			Generate: codegen.GenerateOptions{
 				EchoServer: true,
-				Client:     true,
 				Strict:     true,
 				Models:     true,
 			},
@@ -55,22 +55,47 @@ func Generate(ctx context.Context, specPath string) (err error) {
 	}
 
 	{
-		code, err := codegen.Generate(kinspec, codegen.Configuration{
-			PackageName: "oapi",
-			Generate: codegen.GenerateOptions{
-				Client: true,
-			},
-			OutputOptions: codegen.OutputOptions{
-				UserTemplates: t,
-			},
-		})
-		if err != nil {
-			return fmt.Errorf("fail to generate code: %w", err)
-		}
+		generated := map[*libopenapi.DocumentModel[v3.Document]]struct{}{}
+		for _, pop := range pe.proxied {
+			doc, err := pop.GetOpenAPIDoc()
+			if err != nil {
+				return fmt.Errorf("fail to find upstream openapi doc: %w", err)
+			}
+			docv3, _ := doc.BuildV3Model()
+			if _, ok := generated[docv3]; ok {
+				continue
+			}
 
-		err = os.WriteFile("testdata/gen/oapi-client.go", []byte(code), 0o644)
-		if err != nil {
-			return fmt.Errorf("fail to write generated code: %w", err)
+			spec, err := doc.Render()
+			if err != nil {
+				return fmt.Errorf("fail to render upstream openapi doc: %w", err)
+			}
+			kinspec, err := loadKinDoc(spec)
+			if err != nil {
+				return fmt.Errorf("fail to reload proxy doc with kin: %w", err)
+			}
+			code, err := codegen.Generate(kinspec, codegen.Configuration{
+				PackageName: "oapi",
+				Generate: codegen.GenerateOptions{
+					Client: true,
+					Models: true,
+				},
+				OutputOptions: codegen.OutputOptions{
+					UserTemplates:  t,
+					ClientTypeName: "Client",
+				},
+			})
+			if err != nil {
+				return fmt.Errorf("fail to generate code: %w", err)
+			}
+
+			file := fmt.Sprintf("testdata/gen/oapi-proxy-%s.go", pop.GetName())
+			err = os.WriteFile(file, []byte(code), 0o644)
+			if err != nil {
+				return fmt.Errorf("fail to write generated code: %w", err)
+			}
+
+			generated[docv3] = struct{}{}
 		}
 	}
 
